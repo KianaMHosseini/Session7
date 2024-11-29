a0f6693eed0065b2d4781ccef0a41889cddaf760
Kiana <114957147+KianaMHosseini@users.noreply.github.com>
2024-11-30 00:12:51 +0330

    Add files via upload

---
+++ ir.isc.session7num2/src/ir/isc/session7num2/Read.java
@@ -0,0 +1,22 @@
+package ir.isc.session7num2;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Read {
+
+	public static void main(String[] args) throws StreamReadException, DatabindException, IOException {
+			// TODO Auto-generated method stub
+			ObjectMapper objectMapper = new ObjectMapper();
+
+			File file = new File("data-account.json");
+			Account account = objectMapper.readValue(file, Account.class);
+			System.out.println(account);
+
+		}
+
+	}
